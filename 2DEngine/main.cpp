2c12e9b39383c413d08202ab64cadbf8d3b8b328
Johan Hage <Johan Hage>
2017-05-31 13:41:09 +0200

    Added and tested buffer classes

--- 2DEngine/main.cpp
+++ 2DEngine/main.cpp
@@ -5,17 +5,24 @@
 #include "src\utils\fileutils.h"
 #include "src\graphics\shader.h"
 
+#include "src\graphics\buffers\buffer.h"
+#include "src\graphics\buffers\indexbuffer.h"
+#include "src\graphics\buffers\vertexarray.h"
+
+#define WINDOW_WIDTH	960
+#define WINDOW_HEIGHT	540
 
 int main() {
+
 
 	using namespace engine;
 	using namespace graphics;
 	using namespace maths;
 
-	Window window(800, 600, "2D Engine");
+	Window window(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Engine");
 	glClearColor(0.0, 0.0f, 0.0f, 1.0f);
 
-
+#if 0
 	GLfloat vertices[] = {
 		0, 0, 0,
 		8, 0, 0,
@@ -32,6 +39,44 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
+#else 
+	GLfloat vertices[] = {
+		0, 0, 0,
+		0, 3, 0,
+		8, 3, 0,
+		8, 0, 0
+	};
+
+	GLushort indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	GLfloat colorsA[] = {
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1
+	};
+
+	GLfloat colorsB[] = {
+		0.2f, 0.3f, 0.8f, 1.0f,
+		0.2f, 0.3f, 0.8f, 1.0f,
+		0.2f, 0.3f, 0.8f, 1.0f,
+		0.2f, 0.3f, 0.8f, 1.0f
+	};
+
+	VertexArray sprite1, sprite2;
+	IndexBuffer ibo(indices, 6);
+
+	sprite1.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+	sprite1.addBuffer(new Buffer(colorsA, 4 * 4, 4), 1);
+
+	sprite2.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+	sprite2.addBuffer(new Buffer(colorsB, 4 * 4, 4), 1);
+
+#endif
+
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
@@ -39,13 +84,33 @@ int main() {
 	shader.setUniformMat4("pr_matrix", ortho);
 	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 
-	shader.setUniform2f("light_pos", vec2(8.0f, 4.5f));
+	//shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
 	shader.setUniform4f("col", vec4(0.2f, 0.3f, 0.8f, 1.0f));
 
 
 	while (!window.closed()) {
 		window.clear();
+		double x, y;
+		window.getMousePosition(x, y);
+		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / WINDOW_WIDTH), (float)(9.0f - y * 9.0f / WINDOW_HEIGHT)));
+#if 0
 		glDrawArrays(GL_TRIANGLES, 0, 6);
+#else
+		sprite1.bind();
+		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+		ibo.unbind();
+		sprite1.unbind();
+
+		sprite2.bind();
+		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(0, 0, 0)));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+		ibo.unbind();
+		sprite2.unbind();
+
+#endif
 		window.update();
 
 	}
---
+++ 2DEngine/src/graphics/buffers/vertexarray.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glew.h>
+#include <vector>
+
+#include "buffer.h"
+
+namespace engine {
+	namespace graphics {
+
+		class VertexArray {
+		
+		private:
+
+			GLuint m_ArrayID;
+			std::vector<Buffer*> m_Buffers;
+
+		public:
+			VertexArray();
+			~VertexArray();
+
+			void addBuffer(Buffer* buffer, GLuint index);
+			void bind() const;
+			void unbind() const;
+		};
+
+	}
+}
